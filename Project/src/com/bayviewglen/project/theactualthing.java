aecdf7f476878835dc71a3806564acbf85688564
jjia <jjia@bayviewglen.ca>
2014-09-11 23:51:12 -0400

    .

--- Project/src/com/bayviewglen/project/theactualthing.java
+++ Project/src/com/bayviewglen/project/theactualthing.java
@@ -1,9 +1,28 @@
 package com.bayviewglen.project;
 
-public class theactualthing {
-//jIA
+public class theactualthing extends java.lang.Object
+{
 	//jIA
 	//jIA
 	//jIA
 	//jIA
+	//jIA
+	private int number;
+
+	public void Statistician(){
+		number = 0;
+	}
+
+	public void addAll (Statistician addend){
+			//I don't even know what it means...
+	}
+
+	public void clear()
+	{
+		number = 0;
+	}
+
 }
+
+
+

c659f513a78e495df572d44f9a370110480ce195
jjia <jjia@bayviewglen.ca>
2014-09-12 00:01:57 -0400

    .

--- Project/src/com/bayviewglen/project/theactualthing.java
+++ Project/src/com/bayviewglen/project/theactualthing.java
@@ -7,20 +7,70 @@ public class theactualthing extends java.lang.Object
 	//jIA
 	//jIA
 	//jIA
-	private int number;
+	int number;
+	double sum;
+	double max;
+	double min;
+
+
 
 	public void Statistician(){
 		number = 0;
 	}
 
-	public void addAll (Statistician addend){
-			//I don't even know what it means...
-	}
+	public void addAll(Statistician addend)
+	{
+		if (number == 0) //the number in the original statistican is 0...so the new number is the new array's number
+		{
+			number = addend.count;
+			sum = addend.sum;
+			max = addend.max;
+			min = addend.min;
+		}
+		else
+			number += addend.count;
+			sum += addend.sum;
+			if (addend.max > max)
+				max = addend.max;
+			if (addend.min < min)
+				min = addend.min;
+		}
+	
+
 
 	public void clear()
 	{
 		number = 0;
 	}
+
+	public int length()
+	{
+		return number;
+	}
+	
+	public double maximum()
+	{
+		if (number == 0)
+			return 0;
+		return max;
+	}
+
+	public double mean()
+	{
+		if (number == 0)
+			return 0;
+		return sum / number;
+	}
+
+	public double minimum()
+	{
+		if (number == 0)
+			return 0;
+		return min;
+	}
+
+
+
 
 }
 
